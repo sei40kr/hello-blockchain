e52a850afdb3eb907686822fecf88750ab59a3e6
Seong Yong-ju <sei40kr@gmail.com>
2018-01-13 15:29:29 +0900

    Add blockchain.py
    Add blockchain.py with empty method stubs

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+
+class BlockChain(object):
+    @staticmethod
+    def hash(block):
+        pass
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    @property
+    def last_block(self):
+        pass
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
