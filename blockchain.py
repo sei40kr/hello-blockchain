2dedaac9e9a75346006d53e1a84f638696898c4e
Seong Yong-ju <sei40kr@gmail.com>
2018-01-15 01:01:14 +0900

    Implement blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,21 +1,41 @@
 # coding: utf-8
 
+import hashlib
+import json
+from time import time
+
 
 class BlockChain(object):
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
+        # create a genesis block
+        self.new_block(previous_hash=1, proof=100)
+
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
 
-    def new_transaction(self):
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
